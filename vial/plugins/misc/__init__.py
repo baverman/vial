ed723fadb5b0ee8ca41d7fc65e085c26412bf6b7
Anton Bobrov <bobrov@vl.ru>
2013-09-15 16:19:43 +0400

    misc: custom indent expression VialIndent

--- vial/plugins/misc/__init__.py
+++ vial/plugins/misc/__init__.py
@@ -3,3 +3,4 @@ from vial.utils import lfunc
 
 def init():
     vial.register_command('VialEscape', lfunc('.plugin.escape'))
+    vial.register_function('VialIndent()', lfunc('.plugin.indent'))
--- vial/plugins/misc/plugin.py
+++ vial/plugins/misc/plugin.py
@@ -1,5 +1,5 @@
 from vial import vfunc, vim
-from vial.utils import buffer_with_file, focus_window
+from vial.utils import buffer_with_file, focus_window, vimfunction
 
 def escape():
     if len(vim.windows) < 2:
@@ -18,3 +18,23 @@ def escape():
                 focus_window(cur)
 
             return
+
+def shift_indent(line, shift=1):
+    return vfunc.indent(line-1) + shift * vfunc.eval('&sw')
+
+@vimfunction
+def indent():
+    line = vim.current.window.cursor[0]
+    buf = vim.current.buffer
+    if buf[line-2].endswith(':'):
+        return shift_indent(line)
+
+    cline = buf[line-1].lstrip() 
+    if cline and cline[0] in [')', ']', '}']:
+        return shift_indent(line)
+
+    pline = buf[line-2].rstrip() 
+    if pline and pline[-1] in ['(', '[', '{']:
+        return shift_indent(line)
+
+    return -1
